3359e759fb6c664f928b3b9f43697eb88a03944a
ndom91 <yo@ndo.dev>
2020-09-11 18:38:46 +0200

    update: deets

--- CreateTicketApp.ts
+++ CreateTicketApp.ts
@@ -2,17 +2,19 @@ import {
   ILogger,
   IConfigurationExtend
 } from "@rocket.chat/apps-engine/definition/accessors";
-import {App} from "@rocket.chat/apps-engine/definition/App";
-import {IAppInfo} from "@rocket.chat/apps-engine/definition/metadata/IAppInfo";
+import { App } from "@rocket.chat/apps-engine/definition/App";
+import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata/IAppInfo";
 
-import {CreateTicketCommand} from "./commands/CreateTicketCommand";
-import {TicketPoster} from "./helpers/TicketPoster";
+import { CreateTicketCommand } from "./commands/CreateTicketCommand";
+import { TicketPoster } from "./helpers/TicketPoster";
 
 export class CreateTicketApp extends App {
   private ticketPoster: TicketPoster;
 
   constructor(info: IAppInfo, logger: ILogger) {
     super(info, logger);
+
+    this.ticketPoster = new TicketPoster();
   }
 
   public postTicketPoster(): TicketPoster {
@@ -20,7 +22,7 @@ export class CreateTicketApp extends App {
   }
 
   protected async extendConfiguration(
-    configuration: IConfigurationExtend,
+    configuration: IConfigurationExtend
   ): Promise<void> {
     await configuration.slashCommands.provideSlashCommand(
       new CreateTicketCommand(this)
